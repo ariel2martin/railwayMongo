7704385381a6fe5bf1800e08255e0c27ab9dfa8d
Ariel <ariel2martin@gmail.com>
2022-09-30 13:58:54 -0300

    puerto dinamico

--- server.js
+++ server.js
@@ -1,48 +1,48 @@
 // require de librerías/paquetes
-const express = require('express')
-const { engine } = require('express-handlebars')
-require('dotenv').config()
+const express = require('express');
+const { engine } = require('express-handlebars');
+require('dotenv').config();
 
-const routes = require('./routes')
-const db = require('./db')
+const routes = require('./routes');
+const db = require('./db');
 
 // Setear puerto
-const PORT = 8080
+const PORT = process.env.PORT || 8080;
 
 // llamo a express
-const app = express()
+const app = express();
 
 
 // Configuraciones de Express
 
 app.engine('.hbs', engine({
     extname: '.hbs'
-}))
-app.set('view engine', 'hbs')
+}));
+app.set('view engine', 'hbs');
 
 // Middleware de Express
 // app.use(express.static(__dirname + '/views'))
-app.use(express.static('public'))
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Ruteo
-app.get('/', routes.getRoot)
-app.post('/guardar', routes.postGuardar)
+app.get('/', routes.getRoot);
+app.post('/guardar', routes.postGuardar);
 
 // CONEXIÓN BASE DE DATOS Y LISTEN DEL SERVIDOR
 const arrancar = async () => {
 
     try {
-        await db.conectarDB(db.URL_BASE_DATOS)
-        console.log('Base de datos Conectada')
-        await app.listen(PORT)
-        console.log('Servidor funcionando en el puerto: ' + PORT)
+        await db.conectarDB(db.URL_BASE_DATOS);
+        console.log('Base de datos Conectada');
+        await app.listen(PORT);
+        console.log('Servidor funcionando en el puerto: ' + PORT);
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 
-}
+};
 
-arrancar()
+arrancar();
 
